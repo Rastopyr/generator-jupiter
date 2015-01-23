5df4558738cafafed5a4fb9643cca7d0a4ed6723
rastopyr <rastopyr@gmail.com[C>
2015-01-23 19:54:14 +0200

    0.1.7
    Add prompt file with initial prompts.
    Clean code.
    Add support BaseCluss generator to FixtureBaseGenerator.
    Refact PostgresFixture to Yeoman Base generator.

--- generators/schema/index.js
+++ generators/schema/index.js
@@ -1,64 +1,12 @@
 'use strict';
-var fs = require('fs');
-var path = require('path');
-var join = path.join;
-
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 var _ = require('yeoman-generator/node_modules/lodash');
-var RunContext = require('yeoman-generator/lib/test/run-context');
-var async = require('async');
 
-function getSchemaTypes() {
-  var schemas = fs.readdirSync(join(__dirname, 'schemas'));
+var Base = require('../../utils/BaseClass');
 
-  return _.map(schemas, function (schema) {
-    return _.capitalize(path.basename(schema, path.extname(schema)));
-  });
-}
+var Class = _.extend({
+  entity: 'schema',
+  rootPath: __dirname
+}, Base);
 
-function processResponse(results) {
-  var filePath;
-
-  this.type = this.type || results.type.toLowerCase();
-  this.format = results.format;
-
-  filePath = join(__dirname, 'schemas', this.type);
-
-  this.adapter = require(filePath);
-
-  this.prompt(
-    this.adapter.prompts,
-    this.adapter.processResponse.bind(this)
-  );
-}
-
-module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../../package.json');
-  },
-  prompting: function () {
-    var schemas, prompts;
-
-    this.async();
-
-    this.log(yosay(
-      'Welcome to the ' + chalk.green('JupiterSchema') + ' generator!'
-    ));
-
-    schemas = getSchemaTypes();
-
-    prompts = [{
-      name: 'type',
-      type: 'list',
-      message: 'Select your schema type',
-      choices: schemas
-    }];
-
-    this.prompt(prompts, processResponse.bind(this));
-  },
-  writing: function () {
-    this.fs.copyTpl(this.tplPath, this.destPath, this.tplOptions);
-  }
-});
+module.exports = yeoman.generators.Base.extend(Class);
