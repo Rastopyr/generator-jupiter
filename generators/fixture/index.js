26096574c2feb43e7b247bf904e9dc7f08f60823
rastopyr <rastopyr@gmail.com[C>
2015-01-23 18:07:02 +0200

    Beutify BaseClass.
    Refact Fixture generator with BaseClass.

--- generators/fixture/index.js
+++ generators/fixture/index.js
@@ -1,61 +1,74 @@
 'use strict';
-var fs = require('fs');
-var path = require('path');
-var join = path.join;
-
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 var _ = require('yeoman-generator/node_modules/lodash');
-var RunContext = require('yeoman-generator/lib/test/run-context');
-var async = require('async');
 
-function getFixtureTypes() {
-  var schemas = fs.readdirSync(join(__dirname, 'fixtures'));
+var Base = require('../../utils/BaseClass');
 
-  return _.map(schemas, function (schema) {
-    return _.capitalize(path.basename(schema, path.extname(schema)));
-  });
-}
+var Class = _.extend({
+  entity: 'fixtures',
+  rootPath: __dirname
+}, Base);
 
-function process(results) {
-  var filePath;
+module.exports = yeoman.generators.Base.extend(Class);
 
-  this.type = this.type || results.type.toLowerCase();
+// 'use strict';
+// var fs = require('fs');
+// var path = require('path');
+// var join = path.join;
 
-  filePath = join(__dirname, 'fixtures', this.type);
+// var yeoman = require('yeoman-generator');
+// var chalk = require('chalk');
+// var yosay = require('yosay');
+// var _ = require('yeoman-generator/node_modules/lodash');
+// var RunContext = require('yeoman-generator/lib/test/run-context');
+// var async = require('async');
 
-  this.adapter = require(filePath);
+// function getFixtureTypes() {
+//   var schemas = fs.readdirSync(join(__dirname, 'fixtures'));
 
-  this.prompt(
-    this.adapter.prompts,
-    this.adapter.process.bind(this)(this.async())
-  );
-}
+//   return _.map(schemas, function (schema) {
+//     return _.capitalize(path.basename(schema, path.extname(schema)));
+//   });
+// }
 
-module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../../package.json');
-  },
-  prompting: function () {
-    var fixtures;
+// function process(results) {
+//   var filePath;
 
-    this.async();
+//   this.type = this.type || results.type.toLowerCase();
 
-    this.log(yosay(
-      'Welcome to the ' + chalk.green('JupiterFixture') + ' generator!'
-    ));
+//   filePath = join(__dirname, 'fixtures', this.type);
 
-    fixtures = getFixtureTypes();
+//   this.adapter = require(filePath);
 
-    this.prompt([{
-      name: 'type',
-      type: 'list',
-      message: 'Select your fixture type',
-      choices: fixtures
-    }], process.bind(this));
-  },
-  writing: function () {
-    this.fs.copyTpl(this.tplPath, this.destPath, this.tplOptions);
-  }
-});
+//   this.prompt(
+//     this.adapter.prompts,
+//     this.adapter.process.bind(this)(this.async())
+//   );
+// }
+
+// module.exports = yeoman.generators.Base.extend({
+//   initializing: function () {
+//     this.pkg = require('../../package.json');
+//   },
+//   prompting: function () {
+//     var fixtures;
+
+//     this.async();
+
+//     this.log(yosay(
+//       'Welcome to the ' + chalk.green('JupiterFixture') + ' generator!'
+//     ));
+
+//     fixtures = getFixtureTypes();
+
+//     this.prompt([{
+//       name: 'type',
+//       type: 'list',
+//       message: 'Select your fixture type',
+//       choices: fixtures
+//     }], process.bind(this));
+//   },
+//   writing: function () {
+//     this.fs.copyTpl(this.tplPath, this.destPath, this.tplOptions);
+//   }
+// });
