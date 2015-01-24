48786ca1460648f99cd3ddbe86dfdd4b6f22901f
rastopyr <rastopyr@gmail.com[C>
2015-01-24 17:38:50 +0200

    0.1.12
    Add generators of add/remove column for PostgresMigration subgenerator

---
+++ generators/migration/migrations/postgres/prompts/types/addIndex.js
@@ -0,0 +1,18 @@
+'use strict';
+var chalk = require('chalk');
+
+module.exports = [
+  {
+    name: 'table',
+    message: 'Enter the name of the table that will add index',
+    type: 'input',
+    default: 'table-' + (new Date()).getTime(),
+    required: true
+  }, {
+    name: 'columns',
+    message: 'Enter the name of the columns that will indexed. \n' +
+      chalk.dim('  Type the attribute names separated by commas.\n  At example: firstname,lastname') + ' \n : ',
+    type: 'input',
+    required: true
+  }
+];
