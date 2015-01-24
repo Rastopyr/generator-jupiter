78b97d61ec38175a6c8325626ef4b22c05999f66
rastopyr <rastopyr@gmail.com[C>
2015-01-24 21:11:33 +0200

    Release 0.1.17
    Clean code for style.

--- generators/schema/schemas/postgres/prompts/fields.js
+++ generators/schema/schemas/postgres/prompts/fields.js
@@ -4,13 +4,13 @@ module.exports = [
     name: 'name',
     type: 'input',
     message: 'Type name for new field',
-    default: 'field-'+(new Date).getTime(),
+    default: 'field-' + (new Date()).getTime(),
   },
   {
     name: 'allownull',
     type:'confirm',
     message: "Allow null in field?",
-    default: false,
+    default: false
   },
   {
     name:'type',
--- utils/BaseClass.js
+++ utils/BaseClass.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var util = require('util');
 var path = require('path');
 var fs = require('fs');
 
-var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 var chalk = require('chalk');
 var _ = require('yeoman-generator/node_modules/lodash');
@@ -16,44 +14,6 @@ var inflection = require('underscore.inflection');
  */
 
 _.mixin(inflection.resetInflections());
-
-/*
-  Expose `BaseClass`
- */
-
-module.exports = {
-  initializing: function (args) {;
-    var entities, _this = this;
-
-    this.entities = [];
-
-    this.singular = _.singularize(this.entity);
-    this.plural = _.pluralize(this.entity);
-
-    this.Plural = _.capitalize(this.plural);
-    this.Singular = _.capitalize(this.singular);
-
-    this.log(yosay(
-      'Welcome to the ' + chalk.green('Jupiter'+this.Plural) + ' generator!'
-    ));
-
-    entities = fs.readdirSync(path.join(this.rootPath, this.plural));
-
-    _.map(entities, function (entity) {
-      _this.entities.push(_.capitalize(path.basename(entity, path.extname(entity))));
-    });
-  },
-  prompts: function() {
-    this.async();
-
-    this.prompt({
-      name: 'type',
-      type: 'list',
-      message: 'Select your '+this.singular+' type',
-      choices: this.entities
-    }, process.bind(this));
-  }
-};
 
 /*
   Process prompts of types
@@ -82,3 +42,42 @@ function process(results) {
 
   done();
 }
+
+
+/*
+  Expose `BaseClass`
+ */
+
+module.exports = {
+  initializing: function () {
+    var entities, _this = this;
+
+    this.entities = [];
+
+    this.singular = _.singularize(this.entity);
+    this.plural = _.pluralize(this.entity);
+
+    this.Plural = _.capitalize(this.plural);
+    this.Singular = _.capitalize(this.singular);
+
+    this.log(yosay(
+      'Welcome to the ' + chalk.green('Jupiter' + this.Plural) + ' generator!'
+    ));
+
+    entities = fs.readdirSync(path.join(this.rootPath, this.plural));
+
+    _.map(entities, function (entity) {
+      _this.entities.push(_.capitalize(path.basename(entity, path.extname(entity))));
+    });
+  },
+  prompts: function () {
+    this.async();
+
+    this.prompt({
+      name: 'type',
+      type: 'list',
+      message: 'Select your ' + this.singular + ' type',
+      choices: this.entities
+    }, process.bind(this));
+  }
+};
