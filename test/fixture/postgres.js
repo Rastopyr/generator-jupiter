953a2528f7f705cd1c07e05cbe1481791921d11e
rastopyr <rastopyr@gmail.com[C>
2015-01-27 22:41:36 +0200

    0.1.23
    Add singular for Fixture Postgress Generator

--- test/fixture/postgres.js
+++ test/fixture/postgres.js
@@ -49,6 +49,48 @@ describe('jupiter:fixture', function () {
           helpers.assertFileContent(expectedFilePath, new RegExp('module.exports = exports'));
         });
       });
+
+      describe('singular', function () {
+        var fixture, tmpDir, expectedFilePath;
+
+        tmpDir = path.join(os.tmpdir(), './temp-test');
+
+        fixture = {
+          type: 'Postgres',
+          format: 'coffee',
+          ftype: 'single',
+          name: 'fixture-' + (new Date()).getTime(),
+          filename: 'fixture-' + (new Date()).getTime(),
+          relpath: 'relpath',
+          correct: true
+        };
+
+        expectedFilePath = path.join(
+          'server/application/fixtures/',
+          fixture.relpath,
+          fixture.filename + '.' + fixture.format
+        );
+
+        before(function (done) {
+          helpers.run(path.join(__dirname, '../../generators/fixture/index'))
+            .inDir(tmpDir)
+            .withPrompt(fixture)
+            .on('end', done);
+        });
+
+        it('creates file', function () {
+          assert.file([
+            expectedFilePath
+          ]);
+        });
+
+        it('file content', function () {
+          helpers.assertFileContent(expectedFilePath, new RegExp('name = \"' + fixture.name + '\"'));
+          helpers.assertFileContent(expectedFilePath, new RegExp('data \= null'));
+          helpers.assertFileContent(expectedFilePath, new RegExp('exports = {\n  name\n  data\n}'));
+          helpers.assertFileContent(expectedFilePath, new RegExp('module.exports = exports'));
+        });
+      });
     });
   });
 });
